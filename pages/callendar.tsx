20bdd8b77d1d62e904a2ff134c03aa16311dc572
Habownia <hubertkrz7@gmail.com>
2022-10-26 21:19:38 +0200

    Fixed callendar

--- pages/callendar.tsx
+++ pages/callendar.tsx
@@ -1,10 +1,7 @@
-import { useEffect } from 'react';
 import { NextPage } from 'next';
 import { MongoClient } from 'mongodb';
 
 import Layout from '../components/layout';
-import { ReactNode } from 'react';
-import { exit } from 'process';
 
 type Data = {
 	id: string;
@@ -17,87 +14,28 @@ type Data = {
 
 interface Props {
 	orders: {
+		map(arg: (item: Data) => string): Iterable<unknown> | null | undefined;
 		[index: number]: Data;
 	};
 }
 
-// const Callendar: NextPage<Props> = (props) => {
-const Callendar = () => {
-	const props = {
-		orders: [
-			{
-				_id: null,
-				firstName: 'Joe',
-				lastName: 'Mama',
-				email: 'joe@mama.com',
-				description: 'Joe Mama',
-				date: '22222-02-22',
-				id: '633b2d685f62909e18eaea97',
-			},
-			{
-				_id: null,
-				firstName: 'Joe',
-				lastName: 'Mama',
-				email: 'joe@mama.com',
-				description: 'Joe Mama',
-				date: '2022-03-25',
-				id: '633b2d685f62909e18eaea97',
-			},
-			{
-				_id: null,
-				firstName: 'Joe',
-				lastName: 'Mama',
-				email: 'joe@mama.com',
-				description: 'Joe Mama',
-				date: '22222-02-24',
-				id: '633b2d685f62909e18eaea97',
-			},
-			{
-				_id: null,
-				firstName: 'Joe',
-				lastName: 'Mama',
-				email: 'joe@mama.com',
-				description: 'Joe Mama',
-				date: '22222-02-25',
-				id: '633b2d685f62909e18eaea97',
-			},
-			{
-				_id: null,
-				firstName: 'George',
-				lastName: 'Mama',
-				email: 'george@mama.com',
-				description: 'George mama',
-				date: '22222-02-23',
-				id: '633b2d905f62909e18eaea98',
-			},
-			{
-				_id: null,
-				firstName: 'George',
-				lastName: 'Mama',
-				email: 'george@mama.com',
-				description: 'George mama',
-				date: '22222-02-23',
-				id: '633b2d965f62909e18eaea99',
-			},
-		],
-	};
-
+const Callendar: NextPage<Props> = (props) => {
 	// tworzymy unikalny set dat
-	const unique = [...new Set(props.orders.map((item) => item.date))].filter(
-		(elem) => {
-			const date = new Date(elem).getUTCFullYear();
-			const actualYear = new Date().getUTCFullYear();
-			// sprawdza czy np !(2030 > 2022) i !(1997 < 2022)
-			return !(date > actualYear) && !(date < actualYear) ? true : false;
-		}
-	);
-
-	console.log(unique);
+	const unique = [
+		...new Set(props.orders.map((item: Data) => item.date)),
+	].filter((elem: any) => {
+		const date = new Date(elem).getUTCFullYear();
+		const actualYear = new Date().getUTCFullYear();
+		// sprawdza czy np !(2030 > 2022) i !(1997 < 2022)
+		return !(date > actualYear) && !(date < actualYear) ? true : false;
+	});
 
 	// sortujemy daty w kolejności rosnącej
 	let sortedDates = unique.sort(
-		(a, b) => new Date(a).getTime() - new Date(b).getTime()
+		(a: any, b: any) => new Date(a).getTime() - new Date(b).getTime()
 	);
+
+	// unique => sortedDates => months => monthNames + monthElements
 
 	const monthsNames = [
 		'Styczeń',
@@ -130,7 +68,7 @@ const Callendar = () => {
 	};
 
 	// przydzielamy dni z obiektu do tablic miesięcy
-	sortedDates.forEach((elem) => {
+	sortedDates.forEach((elem: any) => {
 		// dzielimy datę na części
 		const dateChunked = elem.split('-');
 
@@ -140,23 +78,22 @@ const Callendar = () => {
 		months[month].push(day);
 	});
 
-	const monthsElements = monthsNames.map((elem, index) => {
-		const uniqueDays = [...new Set(months[index + 1])].map((elem, index) => (
-			<p key={index}>{elem}</p>
+	const monthsElements = monthsNames.map((monthName, index) => {
+		const daysInMonth = months[index + 1].map((day, index) => (
+			<p key={index}>{day}</p>
 		));
 
 		return (
 			<div key={index} className='flex flex-col items-center font-jakarta'>
-				{/* elem to nazwa miesiąca */}
 				<h2 className='text-2xl font-semibold text-gray-200 pb-3'>
-					{/* {elem} */}
-					{`${index + 1}. ${elem}`}
+					{/* {monthName} */}
+					{`${index + 1}. ${monthName}`}
 				</h2>
-				{uniqueDays.length > 0 && (
+				{daysInMonth.length > 0 && (
 					<div className='flex flex-col items-center'>
 						<p>Zajęte dni:</p>
 						<div className='text-md flex gap-3 flex-wrap max-w-xs'>
-							{uniqueDays}
+							{daysInMonth}
 						</div>
 					</div>
 				)}
@@ -176,26 +113,26 @@ const Callendar = () => {
 	);
 };
 
-// export async function getStaticProps() {
-// 	const client = await MongoClient.connect(
-// 		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@trzycierzbase.3nnsmqr.mongodb.net/orders?retryWrites=true&w=majority`
-// 	);
-// 	const db = client.db();
+export async function getStaticProps() {
+	const client = await MongoClient.connect(
+		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@trzycierzbase.3nnsmqr.mongodb.net/orders?retryWrites=true&w=majority`
+	);
+	const db = client.db();
 
-// 	const ordersCollection = db.collection('orders');
-// 	const orders = await ordersCollection.find().toArray();
+	const ordersCollection = db.collection('orders');
+	const orders = await ordersCollection.find().toArray();
 
-// 	client.close();
-// 	return {
-// 		props: {
-// 			orders: orders.map((item) => ({
-// 				...item,
-// 				id: item._id.toString(),
-// 				_id: null,
-// 			})),
-// 		},
-// 		revalidate: 1,
-// 	};
-// }
+	client.close();
+	return {
+		props: {
+			orders: orders.map((item) => ({
+				...item,
+				id: item._id.toString(),
+				_id: null,
+			})),
+		},
+		revalidate: 1,
+	};
+}
 
 export default Callendar;
